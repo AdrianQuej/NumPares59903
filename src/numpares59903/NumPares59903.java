138fa3f0292ccfdcb9a6706dd6011974972e11e3
Adrian Quej <Adrian Quej@LAPTOP-LCPEV7PP>
2021-02-24 20:34:08 -0600

    PRIMER COMMIT

---
+++ src/numpares59903/NumPares59903.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numpares59903;
+
+/**
+ *
+ * @author Adrian Quej
+ */
+public class NumPares59903 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int numero = 2;
+        int paresTotales = 0;
+        
+        while( paresTotales <= 46 ){
+            
+            System.out.println( numero );
+            
+            numero +=2;
+            
+            paresTotales++;
+        }
+        
+        
+        
+        
+        
+       
+    }
+    
+}
